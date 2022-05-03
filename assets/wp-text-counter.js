429304bf04b7e55abd8dea2c9a9df8ea1c19b2b5
Sachin Rai <sachinkiranti@gmail.com>
2022-05-03 09:30:46 +0545

    feat: process text using button submit instead of blur event

--- assets/wp-text-counter.js
+++ assets/wp-text-counter.js
@@ -9,16 +9,17 @@
         cacheDom: function () {
             this.$wtcWrapper = $('.wp-text-counter-wrapper');
             this.inputel  = this.$wtcWrapper.find('textarea[name=wp-text-counter-element]');
+            this.button  = this.$wtcWrapper.find('#wp-text-counter-btn');
             this.wordCountHolder  = this.$wtcWrapper.find('.wp-text-counter-words');
             this.charCountHolder  = this.$wtcWrapper.find('.wp-text-counter-characters');
         },
 
         bind: function () {
-            this.inputel.on('blur', this.process);
+            this.button.on('click', this.process);
         },
 
         process:function () {
-            var $text = $(this).val();
+            var $text = WTCManager.inputel.val();
             WTCManager.wordCountHolder.html( $text.wordCount() );
             WTCManager.charCountHolder.html( $text.charCount() );
         }
